bcf683d0ab66271c912452ff8ec200b72a5ae254
mayconmrr <maycon.mrr@gmail.com>
2020-04-04 08:39:29 -0600

    Transform app in PWA

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: 'chats#index'
 
+  get "/manifest.json", to: "service_workers#manifest"
+  get "/service-worker.js"  => "service_workers#service_worker"
+
+  resources :contacts, only: %i(index new create destroy)
+
   devise_for :users
   resources :contacts, only: %i(index new create destroy)
   resources :chats, only: %w[index show] do
