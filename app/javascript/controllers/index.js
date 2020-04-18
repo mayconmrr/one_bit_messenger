14f623ecf1e15d3920650f71d192140bd951fa09
mayconmrr <maycon.mrr@gmail.com>
2020-04-18 11:50:15 -0600

    Send message with enter and focus on text area after send message

--- app/javascript/controllers/index.js
+++
@@ -1,5 +0,0 @@
-// Load all the channels within this directory and all subdirectories.
-// Channel files must be named *_channel.js.
-
-const controllers = require.context("controllers", true, /.js$/)
-controllers.keys().forEach(controllers)
